2f7cea24a75bb7e3c742d97f18c73b3e30b56c48
Shravani <badabeshravani@gmail.com>
2026-06-09 22:08:31 +0530

    Updated CORS for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,11 +23,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://crypto-alert-system-one.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
