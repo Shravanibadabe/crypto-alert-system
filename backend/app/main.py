fda02817a4bd44bb273484724e80d958792d5d21
Shravani <badabeshravani@gmail.com>
2026-06-09 22:23:35 +0530

    Fix CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://crypto-alert-system-three.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
